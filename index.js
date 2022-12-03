ee084e8fdde9d8e7b4a1af55c85db8a6c319c409
SparkyStyleSheet <119263280+SparkyStyleSheet@users.noreply.github.com>
2022-12-03 10:43:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.title = 'SPSS';
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = './file/SPSS.ico';
